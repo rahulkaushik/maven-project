059a7fa6cace49cf1bb843cdc89cb7843873a176
Rahul Kaushik <rahul.kaushik@spcapitaliq.com>
2017-01-12 00:28:24 -0500

    Fix spacing issue

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,11 +13,10 @@ public class Greeter {
   }
 
   /**
-   * @param some example
+   * @param someone the name of a person
    * @return greeting string
-  **/
- 
-  public final String greet(final String someone) {
+   */
+   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
